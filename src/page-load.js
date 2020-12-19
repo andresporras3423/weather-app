3e4189ba7a73b895b7ed7914ae1472561fb5e840
oscarporras02 <oscarrussi@outlook.com>
2020-12-18 22:27:55 -0500

    using a select to choose the unit for temperature

--- src/page-load.js
+++ src/page-load.js
@@ -1,19 +1,20 @@
 const pageLoad = () => ({
     divContent : document.getElementById("content"),
     divList : document.createElement("div"),
-    divAdd : document.createElement("div"),
+    divSearch : document.createElement("div"),
     paragraph : document.createElement("p"),
     buttonUpdate : document.createElement("button"),
     headAdd : document.createElement("h3"),
     nameCity : document.createElement("input"),
-    unitTemp : document.createElement("input"),
+    selectUnit : document.createElement("select"),
+    //unitTemp : document.createElement("input"),
     buttonSearch : document.createElement("button"),
   loadPageContent() {
     const that = this;
     this.divContent.classList.add("col-12");
     this.divContent.classList.add("row");
     this.divContent.appendChild(this.divList);
-    this.divContent.appendChild(this.divAdd);
+    this.divContent.appendChild(this.divSearch);
     this.loadDivShowResult();
     this.loadDivSearchCity();
     this.buttonUpdate.onclick= function() {};
@@ -27,18 +28,32 @@ const pageLoad = () => ({
     this.divList.appendChild(this.buttonUpdate);
   },
   loadDivSearchCity() {
-    this.divAdd.classList.add("child-width");
-    this.divAdd.classList.add("col-4");
+    this.divSearch.classList.add("child-width");
+    this.divSearch.classList.add("col-4");
     this.nameCity.type="text";
-    this.unitTemp.type="text";
+    //this.unitTemp.type="text";
     this.headAdd.innerText="Search city";
     this.nameCity.placeholder="City name";
-    this.unitTemp.placeholder="unit temperature";
+    //this.unitTemp.placeholder="unit temperature";
     this.buttonSearch.innerText="Search";
-    this.divAdd.appendChild(this.headAdd);
-    this.divAdd.appendChild(this.nameCity);
-    this.divAdd.appendChild(this.unitTemp);
-    this.divAdd.appendChild(this.buttonSearch);
+    this.divSearch.appendChild(this.headAdd);
+    this.divSearch.appendChild(this.nameCity);
+    //this.divSearch.appendChild(this.unitTemp);
+    this.loadSelectUnit();
+    this.divSearch.appendChild(this.buttonSearch);
+  },
+  loadSelectUnit() {
+    const that = this;
+    //this.labelPriority.innerText = 'Priority: ';
+    let vals=['metric','imperial','standard'];
+    ['Celsius', 'Fahrenheit', 'Kelvin'].forEach((prior, index) => {
+      const opt = document.createElement('option');
+      opt.value = vals[index];
+      opt.innerHTML = prior;
+      that.selectUnit.appendChild(opt);
+    });
+    //this.formTodo.appendChild(this.labelPriority);
+    this.divSearch.appendChild(this.selectUnit);
   },
 //   updateList() {
 //     this.paragraph.innerHTML="";
@@ -59,7 +74,7 @@ const pageLoad = () => ({
 //     });
 //   },
   getWeather() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.nameCity.value}&units=${this.unitTemp.value}&callback=test&appid=a71219e79a6b01978ac3a9f3ffccca37`, {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.nameCity.value}&units=${this.selectUnit.value}&callback=test&appid=a71219e79a6b01978ac3a9f3ffccca37`, {
         method: 'get',
         mode: 'cors'
       })
@@ -70,13 +85,14 @@ const pageLoad = () => ({
         jsonChars = jsonChars.slice(5, jsonChars.length-1).join("");
         let data = JSON.parse(jsonChars);
         console.log(data);
-        this.nameCity.value="";
-        this.unitTemp.value="";
-        this.paragraph.innerText=`longitude: ${data.coord.lon}
+        this.paragraph.innerText=`city: ${this.nameCity.value}
+        longitude: ${data.coord.lon}
         latitude: ${data.coord.lat}
         temperature: ${data.main.temp}
         pressure: ${data.main.pressure}
         humidity: ${data.main.humidity}`;
+        this.nameCity.value="";
+        this.selectUnit.value="metric";
         // Object.values(scores).forEach((score, index)=>{
         //   const label = document.createElement("label");
         //   label.innerText=`${index+1}) user: ${score.user}, score: ${score.score}`;
